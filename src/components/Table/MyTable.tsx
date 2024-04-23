d89fa996c330be92825b95d0135feb3495202e35
fadeyush <a.fadeyush@gmail.com>
2024-04-23 09:41:39 +0300

    поиск по имени

--- src/components/Table/MyTable.tsx
+++ src/components/Table/MyTable.tsx
@@ -1,4 +1,5 @@
-import * as React from 'react';
+import React, { useMemo, useState } from 'react';
+import classes from './Table.module.scss';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -14,6 +15,9 @@ export default function BasicTable() {
   
   const {users, isLoading, error} = useAppSelector(state => state.usersReducer);
   const dispatch = useAppDispatch();
+  const [searchQuery, setSearchQuery] = useState<string>('');
+
+ 
 
   React.useEffect(()=> {
     dispatch(fetchUsers())
@@ -38,42 +42,52 @@ export default function BasicTable() {
     createData(user.id, user.name, user.username, user.email)
   );
 
+  const searchRows = useMemo(()=>{
+    return rows.filter(user=> user.name.toLowerCase().includes(searchQuery.toLowerCase()));
+  }, [searchQuery, users]);
+
   return (
     <>
-      {isLoading ?  
+      { 
+      isLoading ?  
         <h2>Загрузка...</h2> : 
-    error ? 
+      error ? 
         <h2>{error}</h2> 
-    :
-    
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="right">id</TableCell>
-            <TableCell align="right">name</TableCell>
-            <TableCell align="right">username</TableCell>
-            <TableCell align="right">email</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.id}
-              </TableCell>
-              <TableCell align="right">{row.name}</TableCell>
-              <TableCell align="right">{row.username}</TableCell>
-              <TableCell align="right">{row.email}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  }
+      :
+      <section className={classes.users__table}>
+        <div  className={classes.users__searchWrapper}>
+          <input className={classes.users__search} placeholder='name' value = {searchQuery} onChange={e => setSearchQuery(e.target.value)} type='text'></input>
+          <span className={classes.users__searchIkon}></span>
+        </div>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="right">id</TableCell>
+                <TableCell align="right">name</TableCell>
+                <TableCell align="right">username</TableCell>
+                <TableCell align="right">email</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {searchRows.map((row) => (
+                <TableRow
+                  key={row.name}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.id}
+                  </TableCell>
+                  <TableCell align="right">{row.name}</TableCell>
+                  <TableCell align="right">{row.username}</TableCell>
+                  <TableCell align="right">{row.email}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </section>
+      }
     </>
   );
 }
